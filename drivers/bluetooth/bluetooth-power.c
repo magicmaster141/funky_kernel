6aa493491ba7131432f454996572695d01a60da4
Dennis Boyce <dsb9938@yahoo.com>
2012-09-12 10:03:02 -0400

    Update bluetooth to that found in codeaurora's ics-chocolate branch from Chad0989.

--- drivers/bluetooth/bluetooth-power.c
+++ drivers/bluetooth/bluetooth-power.c
@@ -20,12 +20,6 @@
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/rfkill.h>
-
-#if defined (CONFIG_MACH_LGE_I_BOARD)
-#include "../../lge/include/board_lge.h"
-static struct bluetooth_platform_data *bt_platform_data = 0;
-#else /* origin */
-
 static bool previous;
 
 static int bluetooth_toggle_radio(void *data, bool blocked)
@@ -40,25 +34,15 @@ static int bluetooth_toggle_radio(void *data, bool blocked)
 		previous = blocked;
 	return ret;
 }
-#endif
 
-/* LGE_CHANGES_S [taekeun1.kim@lge.com] 2010-06-06, for bt */
-#if defined (CONFIG_MACH_LGE_I_BOARD)
-static struct rfkill_ops bluetooth_power_rfkill_ops;
-#else
 static const struct rfkill_ops bluetooth_power_rfkill_ops = {
 	.set_block = bluetooth_toggle_radio,
 };
-#endif
 
 static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 {
 	struct rfkill *rfkill;
 	int ret;
-#if defined (CONFIG_MACH_LGE_I_BOARD)
-		bluetooth_power_rfkill_ops.set_block = bt_platform_data->bluetooth_toggle_radio;
-#endif
-
 	rfkill = rfkill_alloc("bt_power", &pdev->dev, RFKILL_TYPE_BLUETOOTH,
 			      &bluetooth_power_rfkill_ops,
 			      pdev->dev.platform_data);
@@ -70,10 +54,7 @@ static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 
 	/* force Bluetooth off during init to allow for user control */
 	rfkill_init_sw_state(rfkill, 1);
-#if !defined (CONFIG_MACH_LGE_I_BOARD)	
 	previous = 1;
-#endif
-
 	ret = rfkill_register(rfkill);
 	if (ret) {
 		dev_err(&pdev->dev, "rfkill register failed=%d\n", ret);
@@ -109,10 +90,6 @@ static int __devinit bt_power_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "platform data not initialized\n");
 		return -ENOSYS;
 	}
-
-#if defined (CONFIG_MACH_LGE_I_BOARD)	
-		bt_platform_data = (struct bluetooth_platform_data *)pdev->dev.platform_data;
-#endif
 	ret = bluetooth_power_rfkill_probe(pdev);
 
 	return ret;
