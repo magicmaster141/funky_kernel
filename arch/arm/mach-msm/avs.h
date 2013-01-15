7e3ba8da3b642837cb5bd0fff26e634bc5208e54
Snuzzo <jrracinfan@gmail.com>
2013-01-15 12:50:56 -0800

    Continued VDD cleanup

--- arch/arm/mach-msm/avs.h
+++ arch/arm/mach-msm/avs.h
@@ -18,10 +18,8 @@
 #define VOLTAGE_MAX  CONFIG_CPU_FREQ_MAX_VDD
 #define VOLTAGE_STEP 25
 
-#define VOLTAGE_MIN  CONFIG_CPU_FREQ_MIN_VDD /* mV */
-#define VOLTAGE_MAX  CONFIG_CPU_FREQ_MAX_VDD
-#define VOLTAGE_STEP 25
-
+int __init avs_init(int (*set_vdd)(int), u32 freq_cnt, u32 freq_idx);
+void __exit avs_exit(void);
 
 int avs_adjust_freq(u32 freq_index, int begin);
 
